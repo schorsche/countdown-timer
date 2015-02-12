8f361a02ab7b403e9f476b6999f2db2bdd319766
Georg Grab <ggrab@thereisnobreak.net>
2015-02-12 20:36:00 +0100

    Bug fixes, removed redundand packages

--- countdown-timer.js
+++ countdown-timer.js
@@ -1,8 +1,3 @@
-var date = new Date();
-var curHour = date.getHours();
-var curMin  = date.getMinutes();
-var curSec  = date.getSeconds();
-
 var CountDown = function(options){
 	if (    !options.targetDate
 		 || !options.hours
@@ -37,23 +32,28 @@ CountDown.set = function(selector, value){
 
 CountDown.intervalFunc = function(_this){
 	return function(){
-			expire = _this.options.targetDate.getTime() / 1000 
-			    - _this.options.getStartDate() / 1000;
+		// Seconds to expire
+		expire = _this.options.targetDate.getTime() / 1000 
+		    - _this.options.getStartDate() / 1000;
+		console.log(expire);
 
-			remainingSecs = Math.round(expire % 60) - 1;
-			if (remainingSecs == -1) remainingSecs = 59;
-			remainingMins = Math.floor(expire / 60);
-			remainingHrs  = Math.floor(expire / 3600);
+		// Convert Seconds to expire to hh:mm:ss
+		remainingSecs = Math.round(expire % 60) - 1;
+		if (remainingSecs == -1) remainingSecs = 59;
+		remainingMins = Math.floor((expire-1) / 60) % 60;
+		remainingHrs  = Math.floor((expire-1) / 3600);
 
-			CountDown.set(_this.options.seconds, remainingSecs);
-			CountDown.set(_this.options.minutes, remainingMins);
-			CountDown.set(_this.options.hours, remainingHrs);
+		// Display tick
+		CountDown.set(_this.options.seconds, remainingSecs);
+		CountDown.set(_this.options.minutes, remainingMins);
+		CountDown.set(_this.options.hours, remainingHrs);
 
-			if (remainingSecs == 0 && remainingMins == 0 && remainingHrs == 0){
-				_this.options.onCountDownExpired();
-				clearInterval(_this.iID);
-			}
+		// Detect countdown expiry
+		if (remainingSecs == 0 && remainingMins == 0 && remainingHrs == 0){
+			_this.options.onCountDownExpired();
+			clearInterval(_this.iID);
 		}
+	}
 }
 
 CountDown.prototype.hasElapsed = function(){
@@ -61,8 +61,8 @@ CountDown.prototype.hasElapsed = function(){
 }
 
 CountDown.prototype.set = function(){
-	if (!this.hasElapsed()){
-		throw "CountDownTimerDateException: Starting Date is behind Date.now()"
+	if (!(this.hasElapsed())){
+		throw "CountDownTimerDateException: TargetDate is behind Date.now()"
 	}
 	CountDown.intervalFunc(this)();
 	this.iID = setInterval(CountDown.intervalFunc(this) , this.options.duration);
